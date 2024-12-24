172ccb5c3f79ffbfd691172947a24a42cf180a1e
Manuel <NietoSoftwareDeveloper@outlook.com>
2024-12-24 02:37:57 -0500

    ready for finish

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -25,7 +25,7 @@ const Footer = () => {
           <p className='text-xl font-medium mb-5'>GET IN TOUCH</p>
           <ul className='flex flex-col gap-2 text-gray-600'>
             <li>+1-212-456-7890</li>
-            <li>shivam@gmail.com</li>
+            <li>iubiiun</li>
           </ul>
         </div>
 
@@ -33,7 +33,7 @@ const Footer = () => {
 
       <div>
         <hr />
-        <p className='py-5 text-sm text-center'>Copyright 2024 @ Prescripto.com - All Right Reserved.</p>
+        <p className='py-5 text-sm text-center'>Copyright 2025 NietoDeveloper - All Right Reserved.</p>
       </div>
 
     </div>
