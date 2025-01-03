50b655159c406622755c674fc15295dc3c4d18c5
Manuel <NietoSoftwareDeveloper@outlook.com>
2025-01-02 22:52:48 -0500

    tittle

--- src/components/SpecialityMenu.jsx
+++ src/components/SpecialityMenu.jsx
@@ -5,7 +5,7 @@ import { Link } from 'react-router-dom'
 const SpecialityMenu = () => {
     return (
         <div id='speciality' className='flex flex-col items-center gap-4 py-16 text-[#262626]'>
-            <h1 className='text-3xl font-medium'>Find by Service</h1>
+            <h1 className='text-3xl font-medium'>Services</h1>
             <p className='sm:w-1/3 text-center text-sm'>Simply browse through our list of trusted accountants, schedule your appointment</p>
             <div className='flex sm:justify-center gap-4 pt-5 w-full overflow-scroll '>
                 {specialityData.map((item, index) => (
